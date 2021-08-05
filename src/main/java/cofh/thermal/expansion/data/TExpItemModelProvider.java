8e02622f204eda22c7d9ecb9a46fef80675b48b5
King Lemming <kinglemming@gmail.com>
2021-08-05 14:10:19 -0700

    Block and Item asset restructuring.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermal/expansion/data/TExpItemModelProvider.java
+++ src/main/java/cofh/thermal/expansion/data/TExpItemModelProvider.java
@@ -32,15 +32,15 @@ public class TExpItemModelProvider extends ItemModelProviderCoFH {
 
         DeferredRegisterCoFH<Item> reg = ITEMS;
 
-        generated(reg.getSup("press_coin_die"), CRAFTING);
-        generated(reg.getSup("press_gear_die"), CRAFTING);
-        generated(reg.getSup("press_packing_2x2_die"), CRAFTING);
-        generated(reg.getSup("press_packing_3x3_die"), CRAFTING);
-        generated(reg.getSup("press_unpacking_die"), CRAFTING);
+        generated(reg.getSup("press_coin_die"));
+        generated(reg.getSup("press_gear_die"));
+        generated(reg.getSup("press_packing_2x2_die"));
+        generated(reg.getSup("press_packing_3x3_die"));
+        generated(reg.getSup("press_unpacking_die"));
 
-        generated(reg.getSup("chiller_ball_cast"), CRAFTING);
-        generated(reg.getSup("chiller_ingot_cast"), CRAFTING);
-        generated(reg.getSup("chiller_rod_cast"), CRAFTING);
+        generated(reg.getSup("chiller_ball_cast"));
+        generated(reg.getSup("chiller_ingot_cast"));
+        generated(reg.getSup("chiller_rod_cast"));
     }
 
     private void registerBlockItemModels() {
