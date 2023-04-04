aacd0e25fa9c9a00eb8170c6c6af4bf10ef7efe0
King Lemming <kinglemming@gmail.com>
2023-04-03 23:28:19 -0700

    Furnace Slot adjustment, copper ore recipe changes.

--- src/main/java/cofh/thermal/expansion/inventory/container/machine/MachineFurnaceContainer.java
+++ src/main/java/cofh/thermal/expansion/inventory/container/machine/MachineFurnaceContainer.java
@@ -2,12 +2,12 @@ package cofh.thermal.expansion.inventory.container.machine;
 
 import cofh.core.inventory.container.TileContainer;
 import cofh.lib.inventory.container.slot.SlotCoFH;
-import cofh.lib.inventory.container.slot.SlotRemoveOnly;
 import cofh.lib.inventory.wrapper.InvWrapperCoFH;
 import cofh.thermal.lib.block.entity.Reconfigurable4WayBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.FurnaceResultSlot;
 import net.minecraft.world.level.Level;
 
 import static cofh.thermal.expansion.init.TExpContainers.MACHINE_FURNACE_CONTAINER;
@@ -24,7 +24,7 @@ public class MachineFurnaceContainer extends TileContainer {
 
         addSlot(new SlotCoFH(tileInv, 0, 53, 26));
 
-        addSlot(new SlotRemoveOnly(tileInv, 1, 116, 35));
+        addSlot(new FurnaceResultSlot(player, tileInv, 1, 116, 35));
 
         addSlot(new SlotCoFH(tileInv, 2, 8, 53));
 
