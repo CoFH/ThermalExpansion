e04aba834800579cb0f66da3c137d2aa6cb1aa6d
King Lemming <kinglemming@gmail.com>
2023-12-21 20:14:57 -0800

    Creative Tab Sorting.

--- src/main/java/cofh/thermal/expansion/init/registries/TExpItems.java
+++ src/main/java/cofh/thermal/expansion/init/registries/TExpItems.java
@@ -5,6 +5,7 @@ import cofh.thermal.core.common.item.SlotSealItem;
 
 import static cofh.lib.util.Utils.itemProperties;
 import static cofh.lib.util.constants.ModIds.ID_THERMAL_EXPANSION;
+import static cofh.thermal.core.init.registries.ThermalCreativeTabs.itemsTab;
 import static cofh.thermal.core.util.RegistrationHelper.registerItem;
 
 public class TExpItems {
@@ -15,17 +16,17 @@ public class TExpItems {
 
     public static void register() {
 
-        registerItem("slot_seal", () -> new SlotSealItem(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
+        itemsTab(100, registerItem("slot_seal", () -> new SlotSealItem(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
 
-        registerItem("press_coin_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
-        registerItem("press_gear_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
-        registerItem("press_packing_2x2_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
-        registerItem("press_packing_3x3_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
-        registerItem("press_unpacking_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
+        itemsTab(100, registerItem("press_coin_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
+        itemsTab(100, registerItem("press_gear_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
+        itemsTab(100, registerItem("press_packing_2x2_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
+        itemsTab(100, registerItem("press_packing_3x3_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
+        itemsTab(100, registerItem("press_unpacking_die", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
 
-        registerItem("chiller_ball_cast", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
-        registerItem("chiller_ingot_cast", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
-        registerItem("chiller_rod_cast", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION));
+        itemsTab(100, registerItem("chiller_ball_cast", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
+        itemsTab(100, registerItem("chiller_ingot_cast", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
+        itemsTab(100, registerItem("chiller_rod_cast", () -> new ItemCoFH(itemProperties().stacksTo(1)).setModId(ID_THERMAL_EXPANSION)));
     }
 
 }
